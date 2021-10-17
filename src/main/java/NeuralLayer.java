aa07dc82c34fa03b52f3c9763ad02fd4e7016916
Will Reynolds <billiamreynolds@gmail.com>
2021-10-17 17:20:16 -0500

    renamed neuronCount to totalNeurons

--- src/main/java/NeuralLayer.java
+++ src/main/java/NeuralLayer.java
@@ -13,30 +13,30 @@ public class NeuralLayer {
     }
 
     int inputsPerNeuron;
-    int neuronCount;
+    int totalNeurons;
     double[][] weights;
     InitialWeights initialWeight;
     public final Function<Double, Double> activationFunction, activationFunctionDerivative;
 
-    public NeuralLayer(int inputsPerNeuron, int neuronCount) {
-        this(ActivationFunctions.SIGMOD, InitialWeights.RANDOM, inputsPerNeuron, neuronCount);
+    public NeuralLayer(int inputsPerNeuron, int totalNeurons) {
+        this(ActivationFunctions.SIGMOD, InitialWeights.RANDOM, inputsPerNeuron, totalNeurons);
     }
 
     public NeuralLayer(ActivationFunctions activationFunction, InitialWeights initialWeight,
-                       int inputsPerNeuron, int neuronCount ) {
+                       int inputsPerNeuron, int totalNeurons) {
 
         this.inputsPerNeuron = inputsPerNeuron;
-        this.neuronCount = neuronCount;
+        this.totalNeurons = totalNeurons;
 
         //TODO handle different activation functions
         this.activationFunction = NnMath::sigmod;
         this.activationFunctionDerivative = NnMath::sigmodDerivative;
 
 
-        this.weights = new double[this.inputsPerNeuron][this.neuronCount];
+        this.weights = new double[this.inputsPerNeuron][this.totalNeurons];
 
         for (int i=0; i < this.inputsPerNeuron; i++) {
-            for (int j=0; j < this.neuronCount; j++) {
+            for (int j = 0; j < this.totalNeurons; j++) {
                 weights[i][j] = 2*Math.random() - 1;
                 //TODO handle switch based on initialWeight
             }
@@ -45,7 +45,7 @@ public class NeuralLayer {
 
     public void printWeights() {
         for (int i=0; i < this.inputsPerNeuron; i++) {
-            for (int j=0; j < this.neuronCount; j++) {
+            for (int j = 0; j < this.totalNeurons; j++) {
                 System.out.print(this.weights[i][j] + ", \t");
             }
             System.out.println();

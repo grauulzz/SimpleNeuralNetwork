0e82551303aad614de84b27fe32a919cad7b3173
Will Reynolds <billiamreynolds@gmail.com>
2021-10-17 17:12:55 -0500

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test neural network layer: ");
+        System.out.println("Test neural network layer: ");
         NeuralLayer nn1 = new NeuralLayer(3, 4);
         nn1.printWeights();
     }
